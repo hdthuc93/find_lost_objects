2e072a4fd7b13bca837e84416116b7f225660159
Phong Nguyen <phong.ultra@gmail.com>
2017-10-27 18:13:00 +0700

    Prevent save when has error requires

--- src/client/js/controllers/lost-item-ctrl.js
+++ src/client/js/controllers/lost-item-ctrl.js
@@ -2,6 +2,7 @@ var app = angular.module("findLostObject");
 
 app.controller("lostItemCtrl", ['$scope', '$rootScope', '$http', 'helper', lostItemCtrl]);
 function lostItemCtrl($scope, $rootScope, $http, helper) {
+    $scope.emailPattern = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
     function init() {
         $scope.categoryList = [];
         $scope.locatonList = [];
@@ -47,11 +48,21 @@ function lostItemCtrl($scope, $rootScope, $http, helper) {
         if ($scope.item.categoryId) {
             getCategory($scope.item.categoryId);
         } else {
-            helper.popup.info({ title: "Lỗi", message: "Dữ liệu này không tồn tại.", close: function () { return; } })
+            $scope.category = [];
+            //helper.popup.info({ title: "Lỗi", message: "Dữ liệu này không tồn tại.", close: function () { return; } })
         }
     }
 
     $scope.save = function () {
+        console.log($scope.lostItemForm);
+        if ($scope.lostItemForm.$error.required.length > 0) {
+            $scope.lostItemForm[$scope.lostItemForm.$error.required[0].$name].$touched = true;
+            return false;
+        }
+        if (typeof $scope.lostItemForm.$error.email !== 'undefined' && $scope.lostItemForm.$error.email.length > 0) {
+            $scope.lostItemForm[$scope.lostItemForm.$error.email[0].$name].$touched = true;
+            return false;
+        }
         if ($scope.lostItemForm.$invalid) {
             helper.popup.info({ title: "Lỗi", message: "Vui lòng điền thông tin đầy đủ và chính xác.", close: function () { return; } })
             return;
@@ -59,7 +70,7 @@ function lostItemCtrl($scope, $rootScope, $http, helper) {
         var param = $scope.item;
         $http.post("/api/item", param)
             .then(function (response) {
-                console.log(21211,response);
+                console.log(21211, response);
                 var msg = response.data.success ? "Thêm vật thất lạc thành công." : "Thêm vật thất lạc thất bại, vui lòng kiểm tra lại";
                 helper.popup.info({ title: "Thông báo", message: msg, close: function () { return; } })
             });
