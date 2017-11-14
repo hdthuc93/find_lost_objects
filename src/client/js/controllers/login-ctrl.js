0601c13ffbaf264b8d09af8fcd1b1ce67be572dc
Nam Nguyen <nhnam94@gmail.com>
2017-11-15 02:27:59 +0700

    Update login form in front end

--- src/client/js/controllers/login-ctrl.js
+++ src/client/js/controllers/login-ctrl.js
@@ -5,6 +5,7 @@ function loginCtrl($scope, $cookieStore, $http, $rootScope, $timeout, $location,
   function init() {
     $scope.email = "";
     $scope.password = "";
+
   }
   init();
 
@@ -23,11 +24,25 @@ function loginCtrl($scope, $cookieStore, $http, $rootScope, $timeout, $location,
         expireDate.setDate(expireDate.getDate() + 7);
         // Setting a cookie
         $rootScope.masterToken = data.token;
-        $cookieStore.put('userdata', { loggedIn: true, name: data.name, token: data.token }, { 'expires': expireDate });
+        $cookieStore.put(
+          'userdata',
+          {
+            loggedIn: true,
+            name: data.name,
+            token: data.token
+          },
+          {
+            'expires': expireDate
+          });
         $location.path('/');
       } else {
-        helper.popup.info({ title: "Đăng nhập thất bại", message: "Tên đăng nhập hoặc mật khẩu không đúng, vui lòng thử lại.", close: function () { return; } })
-        $scope.showLoginForm();
+        helper.popup.info({
+          title: "Đăng nhập thất bại",
+          message: "Tên đăng nhập hoặc mật khẩu không đúng, vui lòng thử lại.",
+          close: () => {
+            return;
+          }
+        })
       }
     }, function errorCallback() {
       helper.popup.info({ title: "Lỗi", message: "Xảy ra lỗi trong quá trình thực hiện, vui lòng thử lại.", close: function () { location.reload(); return; } })
