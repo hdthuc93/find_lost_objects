11c70eea636b0d4d3b645c99fd874d5a3b093cda
Nam Nguyen <nhnam94@gmail.com>
2017-12-06 18:34:23 +0700

    update

--- src/server/controllers/login-controller.js
+++ src/server/controllers/login-controller.js
@@ -4,37 +4,37 @@ var jwt = require('jsonwebtoken');
 
 
 function Login(req, res) {
-    Users.findOne({ where: {email: req.body.Email} })
+    Users.findOne({ where: { email: req.body.Email } })
         .then((user) => {
             if (!user) {
-               res.send({success: false, msg: 'Authentication failed. User not found.'});
-            }else {
+                res.send({ success: false, msg: 'Authentication failed. User not found.' });
+            } else {
                 user.validPassword(req.body.Pass, (err, isValid) => {
-                    if(err) {
+                    if (err) {
                         console.log(err);
                     }
-                    if(isValid) {
+                    if (isValid) {
                         let cert = config.secret;
-                        let tokenJWT = jwt.sign({data : user}, cert);
-                        res.send({ success: true,msg: "Login success", token: tokenJWT });
+                        let tokenJWT = jwt.sign({ data: user }, cert);
+                        res.send({ success: true, msg: "Login success", token: tokenJWT });
                     }
                     else {
-                        res.send({success: false, msg: 'Authentication failed. Wrong password.'});
+                        res.send({ success: false, msg: 'Authentication failed. Wrong password.' });
                     }
                 })
             }
         }).catch((err) => {
-        console.log(err);
-        return res.status(500).json({
-            success: false,
-            message: "Failed to get user"
+            console.log(err);
+            return res.status(500).json({
+                success: false,
+                message: "Failed to get user"
+            });
         });
-    });
 }
 
 function Register(req, res) {
     console.log(req.body);
-    Users.findOne({ where: {email: req.body.email} })
+    Users.findOne({ where: { email: req.body.email } })
         .then((user) => {
             let userInsert = {
                 first_name: req.body.FirstName,
@@ -44,17 +44,17 @@ function Register(req, res) {
                 password: req.body.Pass
             };
             if (!user) {
-               Users.create(userInsert)
-                .then((data) =>  res.send({success: true, msg: 'Insert success'}))
-                .catch((err) =>  {
-                console.log(err);
-                return res.status(500).json({
-                    success: false,
-                    message: "Failed to insert user"
-                });
-            });
-            }else {
-               res.send({success: false, msg: 'Email exists.'});
+                Users.create(userInsert)
+                    .then((data) => res.send({ success: true, msg: 'Insert success' }))
+                    .catch((err) => {
+                        console.log(err);
+                        return res.status(500).json({
+                            success: false,
+                            message: "Failed to insert user"
+                        });
+                    });
+            } else {
+                res.send({ success: false, msg: 'Email exists.' });
             }
         })
 }
