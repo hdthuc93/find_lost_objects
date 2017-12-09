5033f06013809334ed452e573959e34c20540cc5
Nam Nguyen <nhnam94@gmail.com>
2017-12-10 03:37:38 +0700

    Add location List + Storage List

--- src/client/js/controllers/location-ctrl.js
+++ src/client/js/controllers/location-ctrl.js
@@ -1,8 +1,47 @@
 var app = angular.module("findLostObject");
 
-app.controller("locationCtrl", ['$scope', '$rootScope', 'helper', locationCtrl]);
-function locationCtrl($scope, $rootScope, helper) {
-     
-    
-
+app.controller("locationCtrl", ['$scope', '$rootScope', '$http', 'helper', locationCtrl]);
+function locationCtrl($scope, $rootScope, $http, helper) {
+  function init() {
+  }
+  init();
+  $scope.locationList = {
+    minRowsToShow: 15,
+    enableSorting: false,
+    enableRowSelection: true,
+    multiSelect: false,
+    enableColumnResizing: true,
+    selectionRowHeaderWidth: 35,
+    columnDefs: [
+      { field: 'no', displayName: 'STT', width: 40 },
+      {
+        field: 'name', displayName: 'Tên địa điểm', minWidth: 140,
+        cellTemplate: '<div class="ui-grid-cell-contents">{{row.entity.name}} - <b>Mã: {{row.entity.locationId}}</b></div>'
+      },
+      { field: 'description', displayName: 'Mô tả', minWidth: 120 },
+    ],
+    onRegisterApi: function (gridApi) {
+      $scope.gridApi = gridApi;
+      gridApi.selection.on.rowSelectionChanged($scope, function (row) {
+        if (row.isSelected) {
+          $scope.selectedRow = row.entity;
+        } else {
+          $scope.selectedRow = null;
+        }
+      });
+    }
+  };
+  $http.get("/api/locations")
+    .then(function (response) {
+      if (response.data.success) {
+        var data = response.data.data;
+        data.forEach(function (e, i) {
+          data[i] = e;
+          data[i].no = i + 1;
+        });
+        $scope.locationList.data = data;
+      } else {
+        $scope.locationList.data = [];
+      }
+    });
 }
--- src/client/js/controllers/storage-ctrl.js
+++ src/client/js/controllers/storage-ctrl.js
@@ -1,8 +1,48 @@
 var app = angular.module("findLostObject");
 
-app.controller("storageCtrl", ['$scope', '$rootScope', 'helper', storageCtrl]);
-function storageCtrl($scope, $rootScope, helper) {
-     
-    
+app.controller("storageCtrl", ['$scope', '$rootScope', '$http', 'helper', storageCtrl]);
+function storageCtrl($scope, $rootScope, $http, helper) {
+  function init() {
+  }
+  init();
 
+  $scope.storageList = {
+    minRowsToShow: 15,
+    enableSorting: false,
+    enableRowSelection: true,
+    multiSelect: false,
+    enableColumnResizing: true,
+    selectionRowHeaderWidth: 35,
+    columnDefs: [
+      { field: 'no', displayName: 'STT', width: 40 },
+      {
+        field: 'name', displayName: 'Tên kho cất giữ', minWidth: 140,
+        cellTemplate: '<div class="ui-grid-cell-contents">{{row.entity.name}} - <b>Mã: {{row.entity.storageId}}</b></div>'
+      },
+      { field: 'description', displayName: 'Mô tả', minWidth: 120 },
+    ],
+    onRegisterApi: function (gridApi) {
+      $scope.gridApi = gridApi;
+      gridApi.selection.on.rowSelectionChanged($scope, function (row) {
+        if (row.isSelected) {
+          $scope.selectedRow = row.entity;
+        } else {
+          $scope.selectedRow = null;
+        }
+      });
+    }
+  };
+  $http.get("/api/storages")
+    .then(function (response) {
+      if (response.data.success) {
+        var data = response.data.data;
+        data.forEach(function (e, i) {
+          data[i] = e;
+          data[i].no = i + 1;
+        });
+        $scope.storageList.data = data;
+      } else {
+        $scope.storageList.data = [];
+      }
+    });
 }
